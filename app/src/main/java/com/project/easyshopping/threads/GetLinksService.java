8e60e47c0f1f3aa918e47c312a09264fd7df1deb
Junaid Ishaq <junaidnumlcs@gmail.com>
2020-03-15 16:19:12 +0500

    First Commit

---
+++ app/src/main/java/com/project/easyshopping/threads/GetLinksService.java
@@ -0,0 +1,90 @@
+package com.project.easyshopping.threads;
+
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.khalid.crawler.entities.CrawlLinks;
+import com.khalid.crawler.interfaces.ICrawlerReportable;
+
+public class GetLinksService implements Runnable{
+	private static final String TAG = "GetLinksService";
+	private String mURL;
+	private ICrawlerReportable report;
+	private com.khalid.crawler.HTTPRequest mRequest;
+	private CrawlLinks mCrawlLinks;
+	
+	/**
+	 * A flag that indicates whether this process should be canceled
+	 */
+	protected boolean cancel = false;
+	
+	public GetLinksService(String url, ICrawlerReportable report) {
+		this.mURL = url;
+		this.report = report;
+		mRequest = new com.khalid.crawler.HTTPRequest(mURL);
+	}
+	
+	public void run()
+	{
+		mCrawlLinks = new CrawlLinks(report);
+		try{	
+			processURL(mURL);
+		}catch (Exception e) {
+			e.printStackTrace();
+			Log.e("run", e.getLocalizedMessage());
+		}
+		
+		report.finished();
+	}
+
+	/**
+	 * Called internally to process a URL
+	 * 
+	 * @param url
+	 *            The URL to be processed.
+	 */
+	public void processURL(String urlString) {
+		try {
+			report.spiderFoundURL(urlString);
+			getWebPage(urlString);
+		} catch (Exception e) {
+			e.printStackTrace();
+			Log.e("processURL", e.getLocalizedMessage());
+		}
+	}
+	
+	/**
+	 * utility function to validate http string response 
+	 *@param1 String: URL
+	 */
+	private void getWebPage(String url) {
+		String response = invokeService(url);
+		  if (!TextUtils.isEmpty(response)) {
+				mCrawlLinks.extractLinks(response, response, url.toString());
+		}
+		}
+	
+	/**
+	 * function which gets web content
+	 *@param1 String: URL
+	 */
+	private String invokeService(String url) {
+		String response = null;
+		try
+		{
+			mRequest.addHeader("Accept-Encoding", "gzip");
+			mRequest.execute(com.khalid.crawler.HTTPRequest.RequestMethod.GET);
+			response = mRequest.getResponseString();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		} 
+		return response;
+	}
+	
+	
+	
+
+	
+}
---
+++ app/src/main/java/com/project/easyshopping/ui/login/LoginActivity.java
@@ -0,0 +1,204 @@
+package com.project.easyshopping.ui.login;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.project.easyshopping.R;
+import com.project.easyshopping.activities.MainActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private LoginViewModel loginViewModel;
+
+    private FirebaseAuth firebaseAuth;
+
+    private FirebaseUser currentUser;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        loginViewModel = ViewModelProviders.of(this, new LoginViewModelFactory())
+                .get(LoginViewModel.class);
+
+        final EditText emailEditText = findViewById(R.id.email);
+        final EditText passwordEditText = findViewById(R.id.password);
+        final Button loginButton = findViewById(R.id.login);
+        final Button registerButton = findViewById(R.id.register);
+        final ProgressBar loadingProgressBar = findViewById(R.id.loading);
+        final ProgressDialog loadingProgressDialog = new ProgressDialog(this);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        currentUser = firebaseAuth.getCurrentUser();
+
+        loginViewModel.getLoginFormState().observe(this, new Observer<LoginFormState>() {
+            @Override
+            public void onChanged(@Nullable LoginFormState loginFormState) {
+                if (loginFormState == null) {
+                    return;
+                }
+                loginButton.setEnabled(loginFormState.isDataValid());
+                registerButton.setEnabled(loginFormState.isDataValid());
+                if (loginFormState.getemailError() != null) {
+                    emailEditText.setError(getString(loginFormState.getemailError()));
+                }
+                if (loginFormState.getPasswordError() != null) {
+                    passwordEditText.setError(getString(loginFormState.getPasswordError()));
+                }
+            }
+        });
+
+        loginViewModel.getLoginResult().observe(this, new Observer<LoginResult>() {
+            @Override
+            public void onChanged(@Nullable LoginResult loginResult) {
+                if (loginResult == null) {
+                    return;
+                }
+                loadingProgressBar.setVisibility(View.GONE);
+                if (loginResult.getError() != null) {
+                    showLoginFailed(loginResult.getError());
+                }
+                if (loginResult.getSuccess() != null) {
+                    updateUiWithUser(loginResult.getSuccess());
+                }
+                setResult(Activity.RESULT_OK);
+
+                //Complete and destroy login activity once successful
+                finish();
+            }
+        });
+
+        TextWatcher afterTextChangedListener = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // ignore
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // ignore
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                loginViewModel.loginDataChanged(emailEditText.getText().toString(),
+                        passwordEditText.getText().toString());
+            }
+        };
+        emailEditText.addTextChangedListener(afterTextChangedListener);
+        passwordEditText.addTextChangedListener(afterTextChangedListener);
+        passwordEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    loginViewModel.login(emailEditText.getText().toString(),
+                            passwordEditText.getText().toString());
+                }
+                return false;
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadingProgressDialog.setTitle("Signing In");
+                loadingProgressDialog.setMessage("Please wait...");
+                loadingProgressDialog.setCanceledOnTouchOutside(true);
+                loadingProgressDialog.show();
+                firebaseAuth.signInWithEmailAndPassword(emailEditText.getText().toString(), passwordEditText.getText().toString())
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    sendUserToMainActivity();
+                                    Toast.makeText(LoginActivity.this, "Logged In Successfully", Toast.LENGTH_SHORT).show();
+                                    loadingProgressDialog.dismiss();
+                                }
+                                else {
+                                    String message = task.getException().getMessage();
+                                    Toast.makeText(LoginActivity.this, "Error : " + message, Toast.LENGTH_LONG).show();
+                                    loadingProgressDialog.dismiss();
+                                }
+                            }
+                        });
+            }
+        });
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadingProgressDialog.setTitle("Creating New Account");
+                loadingProgressDialog.setMessage("Please wait, while we are creating a new accout for you...");
+                loadingProgressDialog.setCanceledOnTouchOutside(true);
+                loadingProgressDialog.show();
+                firebaseAuth.createUserWithEmailAndPassword(emailEditText.getText().toString(), passwordEditText.getText().toString())
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    Toast.makeText(LoginActivity.this, "Account Created Successfully", Toast.LENGTH_SHORT).show();
+                                    loadingProgressDialog.dismiss();
+                                }
+                                else {
+                                    String message = task.getException().getMessage();
+                                    Toast.makeText(LoginActivity.this, "Error : " + message, Toast.LENGTH_LONG).show();
+                                    loadingProgressDialog.dismiss();
+                                }
+                            }
+                        });
+            }
+        });
+    }
+
+    private void loginUser(String email, String password) {
+
+    }
+
+    private void updateUiWithUser(LoggedInUserView model) {
+        String welcome = getString(R.string.welcome) + model.getDisplayName();
+        // TODO : initiate successful logged in experience
+        Toast.makeText(getApplicationContext(), welcome, Toast.LENGTH_LONG).show();
+    }
+
+    private void showLoginFailed(@StringRes Integer errorString) {
+        Toast.makeText(getApplicationContext(), errorString, Toast.LENGTH_SHORT).show();
+    }
+
+    private void sendUserToMainActivity() {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(currentUser != null) {
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
+    }
+}
